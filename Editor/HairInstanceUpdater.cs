e9120bad7af859cdf557f1ce1610febc13f66d11
Lasse Jon Fuglsang Pedersen <volmer@gmail.com>
2021-05-06 12:19:28 +0200

    only step simulation "In Editor" when at least one scene view has "always refresh" enabled, or when an interaction occurs

--- Editor/HairInstanceUpdater.cs
+++ Editor/HairInstanceUpdater.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
@@ -6,6 +7,9 @@ namespace Unity.DemoTeam.Hair
 	public class HairInstanceUpdater
 	{
 		static bool s_initialized = false;
+		static bool s_initializedSceneViews = false;
+
+		static HashSet<SceneView> s_sceneViews = new HashSet<SceneView>();
 
 		[InitializeOnLoadMethod]
 		static void StaticInitialize()
@@ -13,6 +17,7 @@ namespace Unity.DemoTeam.Hair
 			if (s_initialized == false)
 			{
 				EditorApplication.update += ConditionalPlayerLoop;
+				SceneView.duringSceneGui += AddSceneView;
 
 				s_initialized = true;
 			}
@@ -21,6 +26,10 @@ namespace Unity.DemoTeam.Hair
 		static void ConditionalPlayerLoop()
 		{
 			if (Application.isPlaying)
+				return;
+
+			var alwaysRefresh = AnySceneViewAlwaysRefresh();
+			if (alwaysRefresh == false)
 				return;
 
 			foreach (var hairInstance in HairInstance.s_instances)
@@ -33,6 +42,29 @@ namespace Unity.DemoTeam.Hair
 					break;
 				}
 			}
+		}
+
+		static void AddSceneView(SceneView sceneView)
+		{
+			if (s_sceneViews.Contains(sceneView) == false)
+				s_sceneViews.Add(sceneView);
+
+			s_initializedSceneViews = true;
+		}
+
+		static bool AnySceneViewAlwaysRefresh()
+		{
+			s_sceneViews.RemoveWhere(sceneView => (sceneView == null));
+
+			foreach (var sceneView in s_sceneViews)
+			{
+				if (sceneView.sceneViewState.alwaysRefreshEnabled)
+				{
+					return true;
+				}
+			}
+
+			return !s_initializedSceneViews;
 		}
 	}
 }
