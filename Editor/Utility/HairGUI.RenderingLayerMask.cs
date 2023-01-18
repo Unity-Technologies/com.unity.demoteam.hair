452d04d7a852226e7772a8afef9ef985ca66e370
Lasse Jon Fuglsang Pedersen <volmer@gmail.com>
2023-01-18 14:46:56 +0100

    fix ui issue with rendering layer mask (add fallback to guard against rendering layer mask names missing in current render pipeline asset)

--- Editor/Utility/HairGUI.RenderingLayerMask.cs
+++ Editor/Utility/HairGUI.RenderingLayerMask.cs
@@ -19,6 +19,44 @@ namespace Unity.DemoTeam.Hair
 
 	public static partial class HairGUI
 	{
+		static string[] s_renderingLayerMaskNames = new string[32];
+		static string[] s_renderingLayerMaskNamesDefault = new string[]
+		{
+			"Default",
+			"Unused Layer 1",
+			"Unused Layer 2",
+			"Unused Layer 3",
+			"Unused Layer 4",
+			"Unused Layer 5",
+			"Unused Layer 6",
+			"Unused Layer 7",
+			"Unused Layer 8",
+			"Unused Layer 9",
+			"Unused Layer 10",
+			"Unused Layer 11",
+			"Unused Layer 12",
+			"Unused Layer 13",
+			"Unused Layer 14",
+			"Unused Layer 15",
+			"Unused Layer 16",
+			"Unused Layer 17",
+			"Unused Layer 18",
+			"Unused Layer 19",
+			"Unused Layer 20",
+			"Unused Layer 21",
+			"Unused Layer 22",
+			"Unused Layer 23",
+			"Unused Layer 24",
+			"Unused Layer 25",
+			"Unused Layer 26",
+			"Unused Layer 27",
+			"Unused Layer 28",
+			"Unused Layer 29",
+			"Unused Layer 30",
+			"Unused Layer 31",
+			"Unused Layer 32",
+		};
+
 		public static void RenderingLayerMask(Rect position, GUIContent label, SerializedProperty property)
 		{
 			label = EditorGUI.BeginProperty(position, label, property);
@@ -43,10 +81,23 @@ namespace Unity.DemoTeam.Hair
 			var currentPipelineAsset = GraphicsSettings.currentRenderPipeline;
 			if (currentPipelineAsset != null)
 			{
-				var layerMaskNames = currentPipelineAsset.renderingLayerMaskNames;
-				if (layerMaskNames != null)
+				var renderingLayerMaskNames = currentPipelineAsset.renderingLayerMaskNames;
+				if (renderingLayerMaskNames != null)
 				{
-					return EditorGUI.MaskField(position, label, mask, layerMaskNames);
+					int n = s_renderingLayerMaskNames.Length;
+					int j = Mathf.Min(n, renderingLayerMaskNames.Length);
+
+					for (int i = 0; i != j; i++)
+					{
+						s_renderingLayerMaskNames[i] = renderingLayerMaskNames[i];
+					}
+
+					for (int i = j; i != n; i++)
+					{
+						s_renderingLayerMaskNames[i] = s_renderingLayerMaskNamesDefault[i];
+					}
+
+					return EditorGUI.MaskField(position, label, mask, s_renderingLayerMaskNames);
 				}
 			}
 
